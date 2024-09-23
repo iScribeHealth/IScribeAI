b3b785935a81a973d2e17aed70599540aca7f869
Logan Gauthier <logan.paul.gauthier@gmail.com>
2024-09-23 16:54:31 -0500

    Set up bare bones package.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "IScribeAI",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "IScribeAI",
+            targets: ["IScribeAI"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "IScribeAI", 
+            path: "Sources/IScribeAISource.xcframework"
+        )
+    ]
+)
