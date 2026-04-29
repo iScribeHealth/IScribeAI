b2fac34f935d0edd093848332d9a97b46416bae5
Logan Gauthier <logan.gauthier@iscribehealth.com>
2026-04-29 16:01:42 -0500

    Add dependency target to package.

--- Package.swift
+++ Package.swift
@@ -8,13 +8,23 @@ let package = Package(
     products: [
         .library(
             name: "IScribeAI",
-            targets: ["IScribeAI"]
+            targets: ["IScribeAI", "IScribeAIDependencies"]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/amplitude/AmplitudeCore-Swift.git", from: "1.4.1"),
     ],
     targets: [
         .binaryTarget(
-            name: "IScribeAI", 
+            name: "IScribeAI",
             path: "Sources/IScribeAISDK.xcframework.zip"
+        ),
+        .target(
+            name: "IScribeAIDependencies",
+            dependencies: [
+                .product(name: "AmplitudeCoreFramework", package: "AmplitudeCore-Swift"),
+            ],
+            path: "Sources/IScribeAI"
         )
     ]
 )
